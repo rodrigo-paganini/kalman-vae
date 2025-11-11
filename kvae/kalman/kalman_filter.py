6722de72d599c73e12fadd8496019281d3ad43b2
Daniel Paricio <danipaezgz@gmail.com>
2025-11-11 16:31:41 +0100

    Added alpha network

--- kvae/kalman/kalman_filter.py
+++ kvae/kalman/kalman_filter.py
@@ -5,67 +5,137 @@ from torch.distributions import MultivariateNormal
 import matplotlib.pyplot as plt
 
 
-class KalmanFilter(nn.Module):
-    def __init__(self, A, B, C, q_scale, r_scale, mu0, Sigma0):
+class DynamicsParameter(nn.Module):
+    def __init__(self, A, B, C, hidden_lstm=32):
+        """
+        A0: [K, n,n] initial state transition matrix
+        B0: [K, n,m] initial control input matrix
+        C0: [K, p,n] initial observation matrix
+        """
         super().__init__()
-        n = A.size(0)
-        m = B.size(1)
-        p = C.size(0)
+
+        K = A.size(0) 
+        n = A.size(1)
+        m = B.size(2)
+        p = C.size(1)
         self.n, self.m, self.p = n, m, p
 
-        # Trainable parameters
-        # n = state dim
-        # m = control dim
-        # p = observation dim
-        self.A = nn.Parameter(A.clone()) # [n,n]
-        self.B = nn.Parameter(B.clone()) # [n,m]
-        self.C = nn.Parameter(C.clone()) # [p,n]
+        self.A = nn.Parameter(A.clone()) 
+        self.B = nn.Parameter(B.clone())
+        self.C = nn.Parameter(C.clone())
+
+        # LSTM hidden state TODO: hardcoded so far
+        self.lstm_state = None 
+
+        self.lstm = nn.LSTM(
+            input_size=self.p, 
+            hidden_size=hidden_lstm, 
+            num_layers=1, 
+            batch_first=True)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_lstm, hidden_lstm), 
+            nn.Tanh(),
+            )
+        
+        self.head_w = nn.Linear(hidden_lstm, K)
+
+    def reset_state(self):
+        self.lstm_state = None
+
+    def compute_step(self, a_tprev):
+        # If a_tprev is [B,_], unsqueeze to [B,1,_]
+        if a_tprev.dim() == 2: 
+            a_tprev = a_tprev.unsqueeze(1)  
+
+        # Apply LSTM, MLP and head
+        h, new_state = self.lstm(a_tprev, self.lstm_state)
+        f = self.mlp(h)                 
+        self.w = torch.softmax(self.head_w(f), dim=-1).squeeze(1)  
+        
+        # Update LSTM state
+        self.lstm_state = new_state
+
+        # Compute weighted sum of matrices
+        A = torch.einsum('bk,kij->bij', self.w, self.A)
+        B = torch.einsum('bk,knm->bnm', self.w, self.B)
+        C = torch.einsum('bk,kpn->bpn', self.w, self.C)
+
+        return A, B, C 
+        
+
+class KalmanFilter(nn.Module):
+    def __init__(self, q_scale, r_scale, mu0, Sigma0, dyn_params):
+        super().__init__()
+        # Dynamics parameter network
+        self.dyn_params = dyn_params
+
+        n = dyn_params.A.size(1)
+        m = dyn_params.B.size(2)
+        p = dyn_params.C.size(1)
+        self.n, self.m, self.p = n, m, p
+
+        dev = Sigma0.device
+        dtp = Sigma0.dtype
 
         # NOTE:Fixed Q,R
-        self.register_buffer("Q", (q_scale**2) * torch.eye(n)) # [n,n]
-        self.register_buffer("R", (r_scale**2) * torch.eye(p)) # [p,p]
+        self.register_buffer("Q", (q_scale**2) * torch.eye(n, dtype=dtp, device=dev))  # [n,n]
+        self.register_buffer("R", (r_scale**2) * torch.eye(p, dtype=dtp, device=dev))  # [p,p]
+        self.register_buffer("I", torch.eye(n, dtype=dtp, device=dev)) 
 
         # Initial belief  TODO: possibly handle them in filter function?
         self.register_buffer("mu0",    mu0.clone())     # [n]
         self.register_buffer("Sigma0", Sigma0.clone())  # [n,n]
-
-        self.register_buffer("I", torch.eye(n, dtype=Sigma0.dtype, device=Sigma0.device))
-
     
     def filter_step(self, mu_t_t, Sigma_t_t, y_t, u_t):
-        A, B, C, Q, R = self.A, self.B, self.C, self.Q, self.R
+        # [B,n,n], [B,n,m], [B,p,n]
+        batch = y_t.size(0)
+        A, B, C = self.dyn_params.compute_step(y_t)  
+
+        Q = self.Q.expand(batch, -1, -1)
+        R = self.R.expand(batch, -1, -1)
 
         # Inputs: mean and covariance of the filtered believe at t-1
-        mu_tprev_tprev = mu_t_t       # [B,n]
-        Sigma_tprev_tprev = Sigma_t_t # [B,n,n]
+        if mu_t_t.dim() == 2:
+            mu_tprev_tprev = mu_t_t.unsqueeze(-1) # [B,n,1]
+        else:
+            mu_tprev_tprev = mu_t_t               # [B,n,1]
+        if u_t.dim() == 2:
+            u_t = u_t.unsqueeze(-1)               # [B,m,1]
+        else:
+            u_t = u_t                             # [B,m,1]
+        if y_t.dim() == 2:
+            y_t = y_t.unsqueeze(-1)               # [B,p,1]
+        else:
+            y_t = y_t                             # [B,p,1]
 
         # Prediction step - Compute beliefs at time t given data up to t-1
-        # [B,n] = [B,n] @ [n,n].T + [B,m] @ [n,m].T
-        mu_t_tprev    = (mu_tprev_tprev @ A.T) + (u_t @ B.T)
-        # [B,n,n] = [n,n] @ [B,n,n] @ [n,n].T + [n,n]
+        Sigma_tprev_tprev = Sigma_t_t       # [B,n,n]
+        # [B,n,1] = [B,n,n] @ [B,n,1] + [B,n,n] @ [B,m,1]
+        mu_t_tprev    = (A @ mu_tprev_tprev) + (B @ u_t)
+        # [B,n,n] = [B,n,n] @ [B,n,n] @ [B,n,n].T + [B,n,n]
         Sigma_t_tprev = A @ Sigma_tprev_tprev @ A.mT + Q
 
         # Measurement update step
         # Compute beliefs at time t given data up to t
         # Predicted observation
-        # [B,p] = [B,n] @ [p,n].T
-        y_pred = mu_t_tprev @ C.T
-        # Innovation (residual)
-        # [B,p,1] = ([B,p] - [B,p]).unsqueeze(-1)
-        r = (y_t - y_pred).unsqueeze(-1)
+        # [B,p,1] = [B,p,n] @ [B,n,1]
+        y_pred = C @ mu_t_tprev
+        # Innovation (residual) [B,p,1]
+        r = y_t - y_pred
         # Innovation covariance with jitter
-        # [B,p,p] = [p,n] @ [B,n,n] @ [p,n].T + [p,p]
+        # [B,p,p] = [B,p,n] @ [B,n,n] @ [B,p,n].T + [B,p,p]
         S = C @ Sigma_t_tprev @ C.mT + R
         S = 0.5 * (S + S.mT) 
         # Kalman gain (to avoid the inverse .solve is used)
-        # [B,n,p] = [B,n,n] @ [p,n].T
+        # [B,n,p] = [B,n,n] @ [B,p,n].T
         PCT = Sigma_t_tprev @ C.mT
         # [B,n,p] : (solve([B,p,p], [B,n,p].T) -> [B,p,n]).T
         K   = torch.linalg.solve(S, PCT.mT).mT
 
         # Posterior mean
-        # [B,n] = [B,n] + ([B,n,p] @ [B,p,1])
-        mu_t_t    = mu_t_tprev + (K @ r).squeeze(-1)
+        # [B,n] = [B,n] + [B,n,p] @ [B,p,1]
+        mu_t_t    = mu_t_tprev + K @ r
         # Posterior covariance Joseph form (more stable)
         # [B,n,n] = [B,n,n] - [B,n,p] @ [B,n,p].T
         I_KC = self.I - K @ C
@@ -73,8 +143,8 @@ class KalmanFilter(nn.Module):
         Sigma_t_t = 0.5 * (Sigma_t_t + Sigma_t_t.mT)  # ensures symmetry
 
         # Return updated beliefs at time t and predicted beliefs at time t
-        return mu_t_t, Sigma_t_t, mu_t_tprev, Sigma_t_tprev
-    
+        return mu_t_t, Sigma_t_t, mu_t_tprev, Sigma_t_tprev, A, B, C
+
 
     def filter(self, Y, U):
         """
@@ -95,12 +165,18 @@ class KalmanFilter(nn.Module):
         mu    = self.mu0.expand(batch, -1)            # [B,n]
         Sigma = self.Sigma0.expand(batch, -1, -1)     # [B,n,n]
 
+        # Reset dynamic parameter network
+        self.dyn_params.reset_state()
+
+        A_list = []
+        B_list = []
+        C_list = []
         mus_filt = []
         Sigmas_filt = []
         mus_pred = []
         Sigmas_pred = []
         for t in range(T):
-            mu_t_t, Sigma_t_t, mu_t_tprev, Sigma_t_tprev = self.filter_step(mu, Sigma, Y[:, t], U[:, t])
+            mu_t_t, Sigma_t_t, mu_t_tprev, Sigma_t_tprev, A, B, C = self.filter_step(mu, Sigma, Y[:, t], U[:, t])
             # Update for next step
             mu, Sigma = mu_t_t, Sigma_t_t
             # Store results
@@ -108,12 +184,16 @@ class KalmanFilter(nn.Module):
             Sigmas_filt.append(Sigma_t_t)
             mus_pred.append(mu_t_tprev)
             Sigmas_pred.append(Sigma_t_tprev)
+            A_list.append(A)
+            B_list.append(B)
+            C_list.append(C)
 
-        return torch.stack(mus_filt, 1), torch.stack(Sigmas_filt, 1), torch.stack(mus_pred, 1), torch.stack(Sigmas_pred, 1)
+        return torch.stack(mus_filt, 1), torch.stack(Sigmas_filt, 1), torch.stack(mus_pred, 1), torch.stack(Sigmas_pred, 1), \
+                    torch.stack(A_list, 1), torch.stack(B_list, 1), torch.stack(C_list, 1)
 
 
     def smooth_step(self, Sigma_t_t, Sigma_tpost_t, Sigma_tpost_T,
-                      mu_t_t, mu_tpost_t, mu_tpost_T):
+                      mu_t_t, mu_tpost_t, mu_tpost_T, A):
         """
         Kalman smoother backward step computes p(z_t | y_1:T, u_1:T) by
         conditioning on past and future data, which reduces uncertainty.
@@ -129,14 +209,12 @@ class KalmanFilter(nn.Module):
             mu_tpost_T:     [B,n]     mu_t|T      smoothed mean at time t+1
             Sigma_tpost_T:  [B,n,n]   Sigma_t|T   smoothed covariance at time t+1
         """
-        A, B, C, Q, R = self.A, self.B, self.C, self.Q, self.R
-
         # Smoother gain J_t = Sigma_{t|t} A^T (Sigma_{t+1|t})^{-1}
         # [B,n,n] 
         J_t = torch.linalg.solve(Sigma_tpost_t.mT, (Sigma_t_t @ A.mT).mT).mT
         # Smoothed mean
         # [B,n] + [B,n] <- ([B,n,n] @ [B,n,1]) 
-        mu_tpost_T = mu_t_t + (J_t @ (mu_tpost_T - mu_tpost_t).unsqueeze(-1)).squeeze(-1)
+        mu_tpost_T = mu_t_t + (J_t @ (mu_tpost_T - mu_tpost_t))
         # Smoothed covariance
         Sigma_tpost_T = Sigma_t_t + J_t @ (Sigma_tpost_T - Sigma_tpost_t) @ J_t.mT
         Sigma_tpost_T = 0.5 * (Sigma_tpost_T + Sigma_tpost_T.mT) 
@@ -151,13 +229,14 @@ class KalmanFilter(nn.Module):
         batch, T, _ = Y.shape
 
         # First run the filter to get filtered and predicted estimates
-        mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = self.filter(Y, U)
+        mus_filt, Sigmas_filt, mus_pred, Sigmas_pred, A_list, B_list, C_list = self.filter(Y, U)
 
         # Initialize smoothing with last filtered estimate
         # mu_T-1|T-1, Sigma_T-1|T-1 
         mu_T, Sigma_T = mus_filt[:, -1], Sigmas_filt[:, -1]
 
-        mus_smooth, Sigmas_smooth = [mu_T], [Sigma_T]
+        mus_smooth = [mu_T]
+        Sigmas_smooth = [Sigma_T]
         for t in range(T-2, -1, -1):      # t = T-2, …, 0
             mu_t_T, Sigma_t_T = self.smooth_step(
                 Sigmas_filt[:, t],  # Sigma_t_t
@@ -165,7 +244,8 @@ class KalmanFilter(nn.Module):
                 Sigma_T,            # Sigma_tpost_T
                 mus_filt[:, t],     # mu_t_t
                 mus_pred[:, t+1],   # mu_tpost_t
-                mu_T                # mu_tpost_T
+                mu_T,               # mu_tpost_T
+                A_list[:, t+1]      # A_t
                             )
             # Update for next step
             mu_T, Sigma_T = mu_t_T, Sigma_t_T
@@ -175,9 +255,10 @@ class KalmanFilter(nn.Module):
 
         mus_smooth.reverse()
         Sigmas_smooth.reverse()
-        return torch.stack(mus_smooth, 1), torch.stack(Sigmas_smooth, 1)
+        return torch.stack(mus_smooth, 1), torch.stack(Sigmas_smooth, 1), A_list, B_list, C_list
 
-    def elbo(self, mu_t_T, Sigma_t_T, y_t, u_t):
+
+    def elbo(self, mu_t_T, Sigma_t_T, y_t, u_t, A_list, B_list, C_list):
         """
         Compute the Evidence Lower Bound (ELBO)
         Args:
@@ -189,25 +270,38 @@ class KalmanFilter(nn.Module):
             elbo:       [B]       ELBO per batch element
         """
         # TODO: review dimensions
-        B, T, n = mu_t_T.shape
-        
+        B = y_t.size(0)
+        T = y_t.size(1)
+
+        # Reshape mu_t_T to [B,T,n]
+        if mu_t_T.dim() == 4:
+            mu_t_T = mu_t_T.squeeze(-1)   
+        else:
+            mu_t_T = mu_t_T         
+        # Reshape u_t to [B,T,m,1]
+        if u_t.dim() == 3: # [B,T,m,1]  
+            u_t = u_t.unsqueeze(-1)      
+        else:   
+            u_t = u_t 
+
         # Sample from the smoothed distribution - to ensure positive definiteness
         L = torch.linalg.cholesky(Sigma_t_T)
         mvn_smooth = MultivariateNormal(mu_t_T, scale_tril=L)
-        # Sample using reparameterization trick (keep gradients) [B,T,n]
-        z_t = mvn_smooth.rsample()  
+        # Sample using reparameterization trick (keep gradients) [B,T,n,1]
+        z_t = mvn_smooth.rsample() 
 
         # Transition likelihood - prod_{t=2}^T p(z_t|z_{t-1}, u_{t})
-        Az = z_t[:, :-1, :] @ self.A.mT
-        Bu = u_t[:,  1:, :] @ self.B.mT
-        mu_trans = Az + Bu  # [B,T-1,n]
+        Az = A_list[:, 1:] @ z_t[:, :-1].unsqueeze(-1)  # [B,T-1,n,1]
+        Bu = (B_list[:, 1:, :, :] @ u_t[:, 1:, :])      # [B,T-1,n,1]
+        
+        mu_trans = (Az + Bu).squeeze(-1)  # [B,T-1,n]
         # TODO: why trick with z_t_transition - mu_transition? 
         mvn_trans = MultivariateNormal(torch.zeros(self.n), self.Q)
         # [B,T-1]
-        log_prob_trans = mvn_trans.log_prob(z_t[:, :-1, :] - mu_trans) 
+        log_prob_trans = mvn_trans.log_prob((z_t[:, :-1, :] - mu_trans).squeeze(-1)) 
 
         # Emission likelihood - prod_{t=1}^T p(y_t|z_t) [B,T,p]
-        mu_emiss = z_t @ self.C.mT 
+        mu_emiss = (C_list @ z_t.unsqueeze(-1)).squeeze(-1)  # [B,T,p]  
         mvn_emiss = MultivariateNormal(torch.zeros(self.p), self.R)
         log_prob_emiss = mvn_emiss.log_prob(y_t - mu_emiss) # [B,T]
 
@@ -265,12 +359,12 @@ if __name__ == '__main__':
     mu0    = torch.zeros(2, dtype=torch.float32)                     
     Sigma0 = torch.diag(torch.tensor([1000.0, 1000.0], dtype=torch.float32))
 
-    kf = KalmanFilter(A, B, C, eps_a, 3.0, mu0, Sigma0)
+    # kf = KalmanFilter(A, B, C, eps_a, 3.0, mu0, Sigma0)
 
-    model_dtype = A.dtype
-    model_device = A.device
-    Y = torch.from_numpy(z_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
-    U = torch.from_numpy(u_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
+    # model_dtype = A.dtype
+    # model_device = A.device
+    # Y = torch.from_numpy(z_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
+    # U = torch.from_numpy(u_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
 
     # # Compute filter
     # mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = kf.filter(Y, U)
@@ -372,14 +466,14 @@ if __name__ == '__main__':
     r_scale = 4.0     # sqrt of emiss noise      
     
     # Data (batch, time)
+    K = 2  
     dt = 0.1
     n_batch, T = 5000, 100
-    Y, U, X = simulate_rocket_batch(n_batch, T, dt, q_scale=2.0, r_scale=4.0)
+    Y, U, X = simulate_rocket_batch(n_batch, T, dt, 2.0, 4.0)
         
-
-    A_true = torch.tensor([[1., dt],[0., 1.]], dtype=torch.float32)
-    B_true = torch.tensor([[0.5*dt*dt],[dt]], dtype=torch.float32)
-    C_true = torch.tensor([[1., 0.]], dtype=torch.float32)
+    A_true = torch.tensor([[1., dt],[0., 1.]], dtype=torch.float32).repeat(K,1,1)
+    B_true = torch.tensor([[0.5*dt*dt],[dt]], dtype=torch.float32).repeat(K,1,1)
+    C_true = torch.tensor([[1., 0.]], dtype=torch.float32).repeat(K,1,1)
 
     # Initial random guesses
     A0 = torch.randn_like(A_true)
@@ -389,28 +483,31 @@ if __name__ == '__main__':
     mu0    = torch.zeros(2)
     Sigma0 = torch.diag(torch.tensor([100., 100.], dtype=torch.float32))
 
-    kf = KalmanFilter(A0.clone(), B0.clone(), C0.clone(), q_scale, r_scale, mu0, Sigma0)
+    dyn_params = DynamicsParameter(A0.clone(), B0.clone(), C0.clone(), hidden_lstm=32)
+    kf = KalmanFilter(q_scale, r_scale, mu0, Sigma0, dyn_params)
     
     # Testing all functions
     # Compute filter
-    mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = kf.filter(Y, U)
+    mus_filt, Sigmas_filt, mus_pred, Sigmas_pred, A_list, B_list, C_list = kf.filter(Y, U)
     # Compute smoother
-    mus_smooth, Sigmas_smooth = kf.smooth(Y, U)
+    mus_smooth, Sigmas_smooth, A_list, B_list, C_list = kf.smooth(Y, U)
     # Compute ELBO
-    elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U)
+    elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U, A_list, B_list, C_list)
 
     # Optimizer
-    opt = torch.optim.Adam([kf.A, kf.B, kf.C], lr=1e-2, weight_decay=0.0)
+    opt = torch.optim.Adam(dyn_params.parameters(), lr=1e-2, weight_decay=0.0)
     kf.train()
     n_epochs = 200
     losses = []
 
     for epoch in range(1, n_epochs + 1):
         opt.zero_grad()
+        # Reset LSTM state within dynamics parameter network
+        dyn_params.reset_state()
 
         # Forward: smooth then ELBO
-        mus_smooth, Sigmas_smooth = kf.smooth(Y, U)            # [B,T,n], [B,T,n,n]
-        elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U)        # scalar (per-frame normalized)
+        mus_smooth, Sigmas_smooth, A_list, B_list, C_list = kf.smooth(Y, U)            # [B,T,n], [B,T,n,n]
+        elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U, A_list, B_list, C_list)        # scalar (per-frame normalized)
         loss = -elbo                                           # maximize ELBO
 
         # Backward + step
@@ -427,17 +524,17 @@ if __name__ == '__main__':
 
     # TODO: not matching, differences in learned base
     print("\nLearned parameters vs ground truth:")
-    print("A (learned):\n", kf.A.detach().cpu().numpy())
-    print("A (true):\n", A.detach().cpu().numpy())
-    print(f"rel ||Â-A|| / ||A|| = {rel_err(kf.A, A):.4e}")
+    print("A (learned):\n", dyn_params.A.detach().cpu().numpy())
+    print("A (true):\n", A_true.detach().cpu().numpy())
+    print(f"rel ||Â-A|| / ||A|| = {rel_err(dyn_params.A, A_true):.4e}")
 
-    print("\nB (learned):\n", kf.B.detach().cpu().numpy())
-    print("B (true):\n", B.detach().cpu().numpy())
-    print(f"rel ||B̂-B|| / ||B|| = {rel_err(kf.B, B):.4e}")
+    print("\nB (learned):\n", dyn_params.B.detach().cpu().numpy())
+    print("B (true):\n", B_true.detach().cpu().numpy())
+    print(f"rel ||B̂-B|| / ||B|| = {rel_err(dyn_params.B, B_true):.4e}")
+    print("\nC (learned):\n", dyn_params.C.detach().cpu().numpy())
 
-    print("\nC (learned):\n", kf.C.detach().cpu().numpy())
-    print("C (true):\n", C.detach().cpu().numpy())
-    print(f"rel ||Ĉ-C|| / ||C|| = {rel_err(kf.C, C):.4e}")
+    print("C (true):\n", C_true.detach().cpu().numpy())
+    print(f"rel ||Ĉ-C|| / ||C|| = {rel_err(dyn_params.C, C_true):.4e}")
 
     print("Debug")
 
