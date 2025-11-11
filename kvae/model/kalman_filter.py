5c79e5634f996009a54e750a81d4dd97b26225e5
Daniel Paricio <danipaezgz@gmail.com>
2025-11-11 11:38:19 +0100

    Added elbo loss

--- kvae/model/kalman_filter.py
+++ kvae/model/kalman_filter.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.distributions import MultivariateNormal
 import matplotlib.pyplot as plt
+
 
 class KalmanFilter(nn.Module):
     def __init__(self, A, B, C, q_scale, r_scale, mu0, Sigma0):
@@ -26,8 +28,10 @@ class KalmanFilter(nn.Module):
         # Initial belief  TODO: possibly handle them in filter function?
         self.register_buffer("mu0",    mu0.clone())     # [n]
         self.register_buffer("Sigma0", Sigma0.clone())  # [n,n]
-    
 
+        self.register_buffer("I", torch.eye(n, dtype=Sigma0.dtype, device=Sigma0.device))
+
+    
     def filter_step(self, mu_t_t, Sigma_t_t, y_t, u_t):
         A, B, C, Q, R = self.A, self.B, self.C, self.Q, self.R
 
@@ -52,6 +56,7 @@ class KalmanFilter(nn.Module):
         # Innovation covariance with jitter
         # [B,p,p] = [p,n] @ [B,n,n] @ [p,n].T + [p,p]
         S = C @ Sigma_t_tprev @ C.mT + R
+        S = 0.5 * (S + S.mT) 
         # Kalman gain (to avoid the inverse .solve is used)
         # [B,n,p] = [B,n,n] @ [p,n].T
         PCT = Sigma_t_tprev @ C.mT
@@ -61,9 +66,11 @@ class KalmanFilter(nn.Module):
         # Posterior mean
         # [B,n] = [B,n] + ([B,n,p] @ [B,p,1])
         mu_t_t    = mu_t_tprev + (K @ r).squeeze(-1)
-        # Posterior covariance
+        # Posterior covariance Joseph form (more stable)
         # [B,n,n] = [B,n,n] - [B,n,p] @ [B,n,p].T
-        Sigma_t_t = Sigma_t_tprev - K @ PCT.mT 
+        I_KC = self.I - K @ C
+        Sigma_t_t = I_KC @ Sigma_t_tprev @ I_KC.mT + K @ R @ K.mT
+        Sigma_t_t = 0.5 * (Sigma_t_t + Sigma_t_t.mT)  # ensures symmetry
 
         # Return updated beliefs at time t and predicted beliefs at time t
         return mu_t_t, Sigma_t_t, mu_t_tprev, Sigma_t_tprev
@@ -124,13 +131,16 @@ class KalmanFilter(nn.Module):
         """
         A, B, C, Q, R = self.A, self.B, self.C, self.Q, self.R
 
-        # Smoother gain
-        J_t  = torch.linalg.solve(Sigma_tpost_t.mT, A @ Sigma_t_t).mT 
+        # Smoother gain J_t = Sigma_{t|t} A^T (Sigma_{t+1|t})^{-1}
+        # [B,n,n] 
+        J_t = torch.linalg.solve(Sigma_tpost_t.mT, (Sigma_t_t @ A.mT).mT).mT
         # Smoothed mean
-        mu_tpost_T = mu_t_t + (J_t @ (mu_tpost_T - mu_tpost_t).T).squeeze(-1)
+        # [B,n] + [B,n] <- ([B,n,n] @ [B,n,1]) 
+        mu_tpost_T = mu_t_t + (J_t @ (mu_tpost_T - mu_tpost_t).unsqueeze(-1)).squeeze(-1)
         # Smoothed covariance
         Sigma_tpost_T = Sigma_t_t + J_t @ (Sigma_tpost_T - Sigma_tpost_t) @ J_t.mT
-
+        Sigma_tpost_T = 0.5 * (Sigma_tpost_T + Sigma_tpost_T.mT) 
+        
         return mu_tpost_T, Sigma_tpost_T    
 
 
@@ -167,6 +177,57 @@ class KalmanFilter(nn.Module):
         Sigmas_smooth.reverse()
         return torch.stack(mus_smooth, 1), torch.stack(Sigmas_smooth, 1)
 
+    def elbo(self, mu_t_T, Sigma_t_T, y_t, u_t):
+        """
+        Compute the Evidence Lower Bound (ELBO)
+        Args:
+            mu_t_T:     [B,T,n]   smoothed means
+            Sigma_t_T:  [B,T,n,n] smoothed covariances
+            y_t:        [B,T,p]   observations
+            u_t:        [B,T,m]   controls
+        Returns:
+            elbo:       [B]       ELBO per batch element
+        """
+        # TODO: review dimensions
+        B, T, n = mu_t_T.shape
+        
+        # Sample from the smoothed distribution - to ensure positive definiteness
+        L = torch.linalg.cholesky(Sigma_t_T)
+        mvn_smooth = MultivariateNormal(mu_t_T, scale_tril=L)
+        # Sample using reparameterization trick (keep gradients) [B,T,n]
+        z_t = mvn_smooth.rsample()  
+
+        # Transition likelihood - prod_{t=2}^T p(z_t|z_{t-1}, u_{t})
+        Az = z_t[:, :-1, :] @ self.A.mT
+        Bu = u_t[:,  1:, :] @ self.B.mT
+        mu_trans = Az + Bu  # [B,T-1,n]
+        # TODO: why trick with z_t_transition - mu_transition? 
+        mvn_trans = MultivariateNormal(torch.zeros(self.n), self.Q)
+        # [B,T-1]
+        log_prob_trans = mvn_trans.log_prob(z_t[:, :-1, :] - mu_trans) 
+
+        # Emission likelihood - prod_{t=1}^T p(y_t|z_t) [B,T,p]
+        mu_emiss = z_t @ self.C.mT 
+        mvn_emiss = MultivariateNormal(torch.zeros(self.p), self.R)
+        log_prob_emiss = mvn_emiss.log_prob(y_t - mu_emiss) # [B,T]
+
+        # Initial term [B]
+        mvn_init = MultivariateNormal(self.mu0, self.Sigma0)
+        log_prob_init = mvn_init.log_prob(z_t[:,0,:])
+
+        # Entropy term [B,T]
+        entropy = -mvn_smooth.log_prob(z_t)
+
+        # ELBO computation as sum per frame 
+        denom = B * T
+        elbo = (
+            log_prob_trans.sum() +
+            log_prob_emiss.sum() +
+            log_prob_init.sum() +
+            entropy.sum()
+        ) / denom
+        return elbo
+    
 
 ## =================   TEST   ================= ##
 if __name__ == '__main__':
@@ -211,51 +272,56 @@ if __name__ == '__main__':
     Y = torch.from_numpy(z_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
     U = torch.from_numpy(u_meas).to(dtype=model_dtype, device=model_device).unsqueeze(0).unsqueeze(-1)  # [1,N,1]
 
-    mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = kf.filter(Y, U)
+    # # Compute filter
+    # mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = kf.filter(Y, U)
+    # # Compute smoother
+    # mus_smooth, Sigmas_smooth = kf.smooth(Y, U)
+    # # Compute ELBO
+    # elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U)
 
-    mus_smooth, Sigmas_smooth = kf.smooth(Y, U)
+    # print(f"ELBO: {elbo.item():.2f}")
 
-    # Convert tensors to numpy
-    mu_np = mus_filt[0].detach().cpu().numpy()
-    P_np  = Sigmas_filt[0].detach().cpu().numpy()
-    pos_mean = mu_np[:, 0]
-    vel_mean = mu_np[:, 1]
-    pos_std  = np.sqrt(P_np[:, 0, 0])
-    vel_std  = np.sqrt(P_np[:, 1, 1])
-    mu_f_s  = mus_smooth[0].detach().cpu().numpy()       
-    P_f_s   = Sigmas_smooth[0].detach().cpu().numpy()    
-    pos_mean_s = mu_f_s[:, 0]
-    vel_mean_s = mu_f_s[:, 1]
-    pos_std_s  = np.sqrt(P_f_s[:, 0, 0])
-    vel_std_s  = np.sqrt(P_f_s[:, 1, 1])
+    # # Convert tensors to numpy
+    # mu_np = mus_filt[0].detach().cpu().numpy()
+    # P_np  = Sigmas_filt[0].detach().cpu().numpy()
+    # pos_mean = mu_np[:, 0]
+    # vel_mean = mu_np[:, 1]
+    # pos_std  = np.sqrt(P_np[:, 0, 0])
+    # vel_std  = np.sqrt(P_np[:, 1, 1])
+    # mu_f_s  = mus_smooth[0].detach().cpu().numpy()       
+    # P_f_s   = Sigmas_smooth[0].detach().cpu().numpy()    
+    # pos_mean_s = mu_f_s[:, 0]
+    # vel_mean_s = mu_f_s[:, 1]
+    # pos_std_s  = np.sqrt(P_f_s[:, 0, 0])
+    # vel_std_s  = np.sqrt(P_f_s[:, 1, 1])
 
-    # --- Altitude plot ---
-    plt.figure()
-    plt.plot(t, x[:,0], label="reference (altitude)")
-    plt.plot(t, pos_mean, label="KF mean (altitude)")
-    plt.plot(t, pos_mean_s, label="RTS smoothed mean")
-    plt.fill_between(t, pos_mean - pos_std, pos_mean + pos_std, alpha=0.2, label="±1σ")
-    plt.fill_between(t, pos_mean_s - pos_std_s, pos_mean_s + pos_std_s, alpha=0.15, label="smooth ±1σ")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Altitude [m]")
-    plt.title("Altitude: reference vs KF mean ±1σ")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # # --- Altitude plot ---
+    # plt.figure()
+    # plt.plot(t, x[:,0], label="reference (altitude)")
+    # plt.plot(t, pos_mean, label="KF mean (altitude)")
+    # plt.plot(t, pos_mean_s, label="RTS smoothed mean")
+    # plt.fill_between(t, pos_mean - pos_std, pos_mean + pos_std, alpha=0.2, label="±1σ")
+    # plt.fill_between(t, pos_mean_s - pos_std_s, pos_mean_s + pos_std_s, alpha=0.15, label="smooth ±1σ")
+    # plt.xlabel("Time [s]")
+    # plt.ylabel("Altitude [m]")
+    # plt.title("Altitude: reference vs KF mean ±1σ")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
-    # --- Velocity plot ---
-    plt.figure()
-    plt.plot(t, x[:,1], label="reference (velocity)")
-    plt.plot(t, vel_mean, label="KF mean (velocity)")
-    plt.plot(t, vel_mean_s, label="RTS smoothed mean")
-    plt.fill_between(t, vel_mean - vel_std, vel_mean + vel_std, alpha=0.2, label="±1σ")
-    plt.fill_between(t, vel_mean_s - vel_std_s, vel_mean_s + vel_std_s, alpha=0.15, label="smooth ±1σ")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Velocity [m/s]")
-    plt.title("Velocity: reference vs KF mean ±1σ")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # # --- Velocity plot ---
+    # plt.figure()
+    # plt.plot(t, x[:,1], label="reference (velocity)")
+    # plt.plot(t, vel_mean, label="KF mean (velocity)")
+    # plt.plot(t, vel_mean_s, label="RTS smoothed mean")
+    # plt.fill_between(t, vel_mean - vel_std, vel_mean + vel_std, alpha=0.2, label="±1σ")
+    # plt.fill_between(t, vel_mean_s - vel_std_s, vel_mean_s + vel_std_s, alpha=0.15, label="smooth ±1σ")
+    # plt.xlabel("Time [s]")
+    # plt.ylabel("Velocity [m/s]")
+    # plt.title("Velocity: reference vs KF mean ±1σ")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
     def simulate_rocket_batch(B, T, dt, sigma_a_meas=2.0, sigma_z=4.0):
         """
@@ -306,21 +372,72 @@ if __name__ == '__main__':
     r_scale = 4.0     # sqrt of emiss noise      
     
     # Data (batch, time)
-    n_batch, T = 16, 100
-    Y, U, X = simulate_rocket_batch(n_batch, T, dt, 2.0, r_scale)
-        
     dt = 0.1
-    A = torch.tensor([[1., dt],[0., 1.]], dtype=torch.float32)
-    B = torch.tensor([[0.5*dt*dt],[dt]], dtype=torch.float32)
-    C = torch.tensor([[1., 0.]], dtype=torch.float32)
+    n_batch, T = 5000, 100
+    Y, U, X = simulate_rocket_batch(n_batch, T, dt, q_scale=2.0, r_scale=4.0)
+        
+
+    A_true = torch.tensor([[1., dt],[0., 1.]], dtype=torch.float32)
+    B_true = torch.tensor([[0.5*dt*dt],[dt]], dtype=torch.float32)
+    C_true = torch.tensor([[1., 0.]], dtype=torch.float32)
+
+    # Initial random guesses
+    A0 = torch.randn_like(A_true)
+    B0 = torch.randn_like(B_true)
+    C0 = torch.randn_like(C_true)
 
     mu0    = torch.zeros(2)
     Sigma0 = torch.diag(torch.tensor([100., 100.], dtype=torch.float32))
 
-    kf = KalmanFilter(A, B, C, q_scale, r_scale, mu0, Sigma0)
+    kf = KalmanFilter(A0.clone(), B0.clone(), C0.clone(), q_scale, r_scale, mu0, Sigma0)
     
-    # Test
-    mus_t, Sigmas_t, mus_pred, Sigmas_pred = kf.filter(Y, U)
+    # Testing all functions
+    # Compute filter
+    mus_filt, Sigmas_filt, mus_pred, Sigmas_pred = kf.filter(Y, U)
+    # Compute smoother
+    mus_smooth, Sigmas_smooth = kf.smooth(Y, U)
+    # Compute ELBO
+    elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U)
+
+    # Optimizer
+    opt = torch.optim.Adam([kf.A, kf.B, kf.C], lr=1e-2, weight_decay=0.0)
+    kf.train()
+    n_epochs = 200
+    losses = []
+
+    for epoch in range(1, n_epochs + 1):
+        opt.zero_grad()
+
+        # Forward: smooth then ELBO
+        mus_smooth, Sigmas_smooth = kf.smooth(Y, U)            # [B,T,n], [B,T,n,n]
+        elbo = kf.elbo(mus_smooth, Sigmas_smooth, Y, U)        # scalar (per-frame normalized)
+        loss = -elbo                                           # maximize ELBO
+
+        # Backward + step
+        loss.backward()
+        opt.step()
+
+        losses.append(loss.item())
+        if epoch % 20 == 0 or epoch == 1:
+            print(f"[epoch {epoch:03d}]  loss = {loss.item():.4f}  ELBO = {elbo.item():.4f}")
+
+    # Print results 
+    def rel_err(est, true):
+        return (torch.linalg.norm(est - true) / torch.linalg.norm(true)).item()
+
+    # TODO: not matching, differences in learned base
+    print("\nLearned parameters vs ground truth:")
+    print("A (learned):\n", kf.A.detach().cpu().numpy())
+    print("A (true):\n", A.detach().cpu().numpy())
+    print(f"rel ||Â-A|| / ||A|| = {rel_err(kf.A, A):.4e}")
+
+    print("\nB (learned):\n", kf.B.detach().cpu().numpy())
+    print("B (true):\n", B.detach().cpu().numpy())
+    print(f"rel ||B̂-B|| / ||B|| = {rel_err(kf.B, B):.4e}")
+
+    print("\nC (learned):\n", kf.C.detach().cpu().numpy())
+    print("C (true):\n", C.detach().cpu().numpy())
+    print(f"rel ||Ĉ-C|| / ||C|| = {rel_err(kf.C, C):.4e}")
 
     print("Debug")
 
